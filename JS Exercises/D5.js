0b39951be776e920658b90400100fd6395fca1e0
Mariam Azfar Azami <azami.maria89@gmail.com>
2024-11-11 20:41:07 +0100

    End JS

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -98,12 +98,22 @@ console.log(justTrims);
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
+
+for( let i= 0; i < cars.length; i++){
+  if (cars[i].color[0]==='b'){
+    console.log('friz');
+  }else{
+    console.log('buzz');
+  }
+}
+
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
+
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
